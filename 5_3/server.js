dae0349d4a5989aa08fa690737f22cd6793d7dac
Taporpone <taporpone@gmail.com>
2017-03-02 14:58:01 +0100

    Init

---
+++ 5_3/server.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var fileContent;
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function(req,res){
+  fs.readFile('./test.json','utf-8', function(err,data){
+    if (err) throw err;
+    fileContent = data;
+    res.send(data);
+  })
+});
+
+app.post('/updateNote/:note', function(req,res){
+  fileContent += req.params.note;
+  fs.writeFile('./test.json',fileContent, function(err){
+    if (err) throw err;
+    console.log('File updated!');
+  })
+});
+
+
+
+var server = app.listen(3000);
