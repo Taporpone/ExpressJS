8317dff87ad052ba0a22d212d71ff4bd80fdffd3
Taporpone <taporpone@gmail.com>
2017-03-28 14:09:32 +0200

    Added error handling

--- 5_3/server.js
+++ 5_3/server.js
@@ -5,32 +5,42 @@ var bodyParser = require('body-parser');
 var app = express();
 var fileContent;
 
-fs.stat('./test.json', function(err){
-  if (err){
-    fs.writeFile('./test.json','', function(err){
-      if (err) throw err;
+fs.stat('./test.json', function (err) {
+  if (err) {
+    fs.writeFile('./test.json', '', function (err) {
+      if (err) {
+        console.log('I couldn\'t create json file. Error: ' + err)
+      }
     });
   }
 });
 
 app.use(bodyParser.json());
 
-app.get('/getNote', function(req,res){
-  fs.readFile('./test.json','utf-8', function(err,data){
-    if (err) throw err;
-    fileContent = data;
-    res.send(data);
+app.get('/getNote', function (req, res) {
+  fs.readFile('./test.json', 'utf-8', function (err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error reading json file :(');
+    } else {
+      fileContent = data;
+      res.send(data);
+    }
     res.end();
   })
 });
 
-app.post('/updateNote/:note', function(req,res){
+app.post('/updateNote/:note', function (req, res) {
   fileContent += req.params.note;
-  fs.writeFile('./test.json',fileContent, function(err){
-    if (err) throw err;
-    console.log('File updated!');
+  fs.writeFile('./test.json', fileContent, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error writing to json file :(');
+      res.end();
+    } else {
+      console.log('File updated!');
+    }
   })
-  res.end();
 });
 
 
